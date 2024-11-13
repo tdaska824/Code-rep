ecf5207837c29fb07e559d3da561d902966cb6fc
tdaska824 <theopdask@gmail.com>
2024-11-13 17:38:51 +0200

    Add ART23 Telemetry v1.0 folder

---
+++ ART23 Telemetry v1.0/Form2.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form2
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Focus()
+        ShowPacketButton.BringToFront()
+    End Sub
+
+    Private Sub ShowPacketButton_Click(sender As Object, e As EventArgs) Handles ShowPacketButton.Click
+        Me.Focus()
+        Me.Width = 265
+        ShowPacketButton.SendToBack()
+        HidePacketButton.BringToFront()
+    End Sub
+
+    Private Sub HidePacketButton_Click(sender As Object, e As EventArgs) Handles HidePacketButton.Click
+        Me.Focus()
+        Me.Width = 184
+        HidePacketButton.SendToBack()
+        ShowPacketButton.BringToFront()
+    End Sub
+End Class
